383b93cd3dc8dce1d36604a29c05506684fe6cc8
rafi <rafitanjibulhasan@gmail.com>
2020-12-11 01:42:15 +0600

    Updates

---
+++ precache-manifest.454e863584e5b2ca51dc1479332fb49c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d193891e51809672ec36",
+    "url": "/Alan-Ai-News-Application/static/css/main.ba097750.chunk.css"
+  },
+  {
+    "revision": "d193891e51809672ec36",
+    "url": "/Alan-Ai-News-Application/static/js/main.30c76af0.chunk.js"
+  },
+  {
+    "revision": "f00b6568d556c6cbcbfa",
+    "url": "/Alan-Ai-News-Application/static/js/runtime~main.9e6a3c8c.js"
+  },
+  {
+    "revision": "db107ebff85322bd29d2",
+    "url": "/Alan-Ai-News-Application/static/js/2.f743b162.chunk.js"
+  },
+  {
+    "revision": "249223b82c1bc62d6797139aec30b997",
+    "url": "/Alan-Ai-News-Application/index.html"
+  }
+];
